6739b58edb025968cc2393bbf57cf8d40e272237
TerrorBite <terrorbite@lethargiclion.net>
2012-09-27 10:03:01 +1000

    New Database interface (instead of abstract class)

--- src/main/java/net/lethargiclion/informaban/persistence/Database.java
+++ src/main/java/net/lethargiclion/informaban/persistence/Database.java
@@ -1,11 +1,13 @@
 package net.lethargiclion.informaban.persistence;
 
-import java.util.Date;
+import java.net.InetAddress;
+import java.util.List;
 
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public abstract class Database {
+import net.lethargiclion.informaban.Enforcement;
+
+public interface Database {
     
     public enum RecordType {
         /**
@@ -30,98 +32,53 @@ public abstract class Database {
         JAIL;
     }
 
-    public abstract boolean isBanned(String username);
+    /**
+     * Make a record of an Enforcement.
+     * @param e The Enforcement to record.
+     * @return true if successful.
+     */
+    public boolean record(Enforcement e);
     
     /**
-     * Makes a record of an event that occurred at the current time.
-     * @param subject The username of the player who was kicked, banned etc.
-     * @param issuer The username of the player who issued the event.
-     * @param reason The reason supplied by the issuer.
-     * @param duration The duration that the ban should remain in force.
-     * @param playerip The string representation of the player's IP address at the time of the event.
-     * @return true if the record was successfully created.
+     * Get all current (i.e. in-effect) Enforcements for the named player.
+     * @param subject The player to look up.
+     * @return
      */
-    protected abstract boolean recordEntry(RecordType type, String subject, String issuer, String reason, int duration, String playerip);
+    public List<Enforcement> getCurrent(String subject);
     
     /**
-     * Makes a record of an event that occurred at an arbritrary time.
-     * @param subject The username of the player who was kicked, banned etc.
-     * @param issuer The username of the player who issued the event.
-     * @param reason The reason supplied by the issuer.
-     * @param duration The duration that the ban should remain in force.
-     * @param playerip The string representation of the player's IP address at the time of the event.
-     * @param date The date and time that this event occurred.
-     * @return true if the record was successfully created.
+     * Get all Enforcements, current or past, for the named player.
+     * @param subject The player to look up.
+     * @return
      */
-    protected abstract boolean recordEntry(RecordType type, String subject, String issuer, String reason, int duration, String playerip, Date date);
+    public List<Enforcement> getAll(String subject);
     
     /**
-     * Records a kick event into the database.
-     * @param sender The CommandSender that executed the kick command.
-     * @param subject The player being kicked.
-     * @param reason The reason supplied for the kick.
-     * @return true if recorded successfully.
+     * Gets all Enforcements that were set by the named player.
+     * @param enforcer
+     * @return
      */
-    public boolean recordKick(CommandSender sender, Player subject, String reason) {
-        
-        return recordEntry(RecordType.KICK, sender.getName(), subject.getName(), reason, 0,
-                subject.getAddress().getAddress().getHostAddress());
-    }
+    public List<Enforcement> getIssuedBy(String enforcer);
     
     /**
-     * Records a ban event into the database.
-     * @param sender The CommandSender that executed the ban command.
-     * @param subject The player being banned.
-     * @param reason The reason supplied for the ban.
-     * @param duration The length of the ban, in seconds, or -1 for a permanent ban.
-     * @return true if recorded successfully.
+     * Checks if the given player is currently banned, either by name or by IP.
+     * @param subject The online Player to check.
+     * @return
      */
-    public boolean recordBan(CommandSender sender, Player subject, String reason, int duration) {
-        
-        return recordEntry(RecordType.BAN, sender.getName(), subject.getName(), reason, duration,
-                subject.getAddress().getAddress().getHostAddress());
-    }
-
-     /**
-     * Records a player IP ban event into the database.
-     * @param sender The CommandSender that executed the ban command.
-     * @param subject The player being banned.
-     * @param reason The reason supplied for the ban.
-     * @param duration The length of the ban, in seconds, or -1 for a permanent ban.
-     * @return true if recorded successfully.
-     */
-    public boolean recordIpBan(CommandSender sender, Player subject, String reason, int duration) {
-        
-        return recordEntry(RecordType.BAN, sender.getName(), subject.getName(), reason, duration,
-                subject.getAddress().getAddress().getHostAddress());
-    }
+    public boolean isBanned(Player subject);
     
     /**
-    * Records a player IP ban event into the database.
-    * @param sender The CommandSender that executed the ban command.
-    * @param ip The IP or range being banned, in dotted-quad or CIDR form.
-    * @param reason The reason supplied for the ban.
-    * @param duration The length of the ban, in seconds, or -1 for a permanent ban.
-    * @return true if recorded successfully.
-    */
-   public boolean recordIpBan(CommandSender sender, String ip, String reason, int duration) {
-       
-       return recordEntry(RecordType.BAN, sender.getName(), null, reason, duration, ip);
-   }
+     * Checks if the given address is banned.
+     * @param addr The address to check.
+     * @return
+     */
+    public boolean isIpBanned(InetAddress addr);
     
-   /**
-    * For future use.
-    * Records a jail event into the database.
-    * @param sender The CommandSender that executed the jail command.
-    * @param subject The player being banned.
-    * @param reason The reason supplied for the ban.
-    * @param duration The length of the ban, in seconds, or -1 for a permanent ban.
-    * @return true if recorded successfully.
-    */
-    public boolean recordJail(CommandSender sender, Player subject, String reason, int duration) {
-        
-        return recordEntry(RecordType.JAIL, sender.getName(), subject.getName(), reason, duration,
-                subject.getAddress().getAddress().getHostAddress());
-    }
+    /**
+     * For future expansion. Checks if the given player is currently jailed.
+     * @param subject The online Player to check.
+     * @return
+     */
+    public boolean isJailed(Player subject);
     
 }
