7aa036a3d59a55930723afd557c0f0cba1d8ea5f
TerrorBite <terrorbite@lethargiclion.net>
2012-09-27 10:37:16 +1000

    Add javadocs to packages

---
+++ src/main/java/net/lethargiclion/informaban/enforcement/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains various classes to facilitate recording and enforcing bans, kicks and jails.
+ *
+ * @author TerrorBite
+ */
+package net.lethargiclion.informaban.enforcement;
---
+++ src/main/java/net/lethargiclion/informaban/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the core Informaban class files.
+ * 
+ * @author "TerrorBite"
+ */
+package net.lethargiclion.informaban;
